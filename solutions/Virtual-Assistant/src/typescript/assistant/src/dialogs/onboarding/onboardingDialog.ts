1057878d083ffb0ebe4e16347ab37e456d6eefe0
Martin Battaglino <martinbatta32@gmail.com>
2019-02-14 16:15:22 -0300

    Add onBoarding Dialog

--- solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingDialog.ts
+++ solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingDialog.ts
@@ -1,34 +1,105 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
-import { ComponentDialog,
-        DialogTurnResult,
-        WaterfallDialog,
-        WaterfallStepContext} from 'botbuilder-dialogs';
+import {
+    BotTelemetryClient,
+    StatePropertyAccessor,
+    TurnContext } from 'botbuilder';
+import {
+    DialogTurnResult,
+    TextPrompt,
+    WaterfallDialog,
+    WaterfallStepContext } from 'botbuilder-dialogs';
+import { BotServices } from '../../botServices';
+import { EnterpriseDialog } from '../shared/enterpriseDialog';
 import { OnboardingResponses } from './onboardingResponses';
+import { IOnboardingState } from './onboardingState';
 
-export class OnboardingDialog extends ComponentDialog {
+export class OnboardingDialog extends EnterpriseDialog {
+    // Fields
+    private static readonly responder: OnboardingResponses = new OnboardingResponses();
+    private accessor: StatePropertyAccessor<IOnboardingState>;
+    private state!: IOnboardingState;
+    private dialogIds: DialogIds = new DialogIds();
 
-    // Declare here the type of properties
-    private static readonly RESPONDER: OnboardingResponses = new OnboardingResponses();
-
-    // Initialize the dialog class properties
-    constructor() {
-        super(OnboardingDialog.name);
+    // Constructor
+    constructor(botServices: BotServices, accessor: StatePropertyAccessor<IOnboardingState>, telemetryClient: BotTelemetryClient) {
+        super(botServices, OnboardingDialog.name, telemetryClient);
+        this.accessor = accessor;
         this.initialDialogId = OnboardingDialog.name;
-
         // tslint:disable-next-line:no-any
-        const value: ((sc: WaterfallStepContext<{}>) => Promise<DialogTurnResult<any>>)[] = [
-            this.end.bind(this)
+        const onboarding: ((sc: WaterfallStepContext<{}>) => Promise<DialogTurnResult<any>>)[] = [
+            this.askForName.bind(this),
+            this.askForLocation.bind(this),
+            this.finishOnboardingDialog.bind(this)
         ];
-
-   // Add here the waterfall dialog
-        this.addDialog(new WaterfallDialog(this.initialDialogId, value));
+        this.addDialog(new WaterfallDialog(this.initialDialogId, onboarding));
+        this.addDialog(new TextPrompt(this.dialogIds.namePrompt));
+        this.addDialog(new TextPrompt(this.dialogIds.locationPrompt));
     }
 
-    // Add here end dialog waterfall.
-    private async end(sc: WaterfallStepContext): Promise<DialogTurnResult> {
-
-        return sc.endDialog(<boolean> sc.result);
+    private async askForName(sc: WaterfallStepContext): Promise<DialogTurnResult> {
+        return sc.prompt(this.dialogIds.namePrompt, {
+            prompt: await OnboardingDialog.responder.renderTemplate
+            (
+                sc.context,
+                OnboardingResponses.responseIds.NamePrompt,
+                'en'
+            )
+        });
     }
+
+    private async askForLocation(sc: WaterfallStepContext): Promise<DialogTurnResult> {
+        this.state = await this.getStateFromAccessor(sc.context);
+        this.state.name = <string> sc.result;
+
+        return sc.prompt(this.dialogIds.locationPrompt, {
+            prompt: await OnboardingDialog.responder.renderTemplate
+            (
+                sc.context,
+                OnboardingResponses.responseIds.LocationPrompt,
+                'en',
+                this.state.name
+            )
+        });
+    }
+
+    private async finishOnboardingDialog(sc: WaterfallStepContext): Promise<DialogTurnResult> {
+        this.state = await this.getStateFromAccessor(sc.context);
+        this.state.location = <string>  sc.result;
+
+        await OnboardingDialog.responder.replyWith(
+            sc.context,
+            OnboardingResponses.responseIds.HaveLocation,
+            this.state.location
+        );
+        await OnboardingDialog.responder.replyWith(
+            sc.context,
+            OnboardingResponses.responseIds.AddLinkedAccountsMessage
+        );
+
+        return sc.endDialog();
+    }
+
+    private async getStateFromAccessor(context: TurnContext): Promise<IOnboardingState>  {
+        const state: IOnboardingState | undefined = await this.accessor.get(context);
+        if (!state) {
+            const newState: IOnboardingState = {
+                dialogStack: [],
+                location: '',
+                name: ''
+            };
+            await this.accessor.set(context, newState);
+
+            return newState;
+        }
+
+        return state;
+    }
+}
+
+class DialogIds {
+    // Constants
+    public readonly namePrompt: string = 'namePrompt';
+    public readonly locationPrompt: string = 'locationPrompt';
 }
--- solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingResponses.ts
+++ solutions/Virtual-Assistant/src/typescript/assistant/src/dialogs/onboarding/onboardingResponses.ts
@@ -1,32 +1,92 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
+import {
+    ActionTypes,
+    Activity,
+    Attachment,
+    CardFactory,
+    CardImage,
+    InputHints,
+    MessageFactory,
+    TurnContext } from 'botbuilder';
 import * as i18n from 'i18n';
 import {
     DictionaryRenderer,
     LanguageTemplateDictionary,
-    TemplateFunction } from '../templateManager/dictionaryRenderer';
+    TemplateFunction,
+    TemplateIdMap} from '../templateManager/dictionaryRenderer';
 import { TemplateManager } from '../templateManager/templateManager';
 
 export class OnboardingResponses extends TemplateManager {
-
-    // Declare here the type of properties and the prompts
-    public static RESPONSE_IDS: {
+    public static responseIds: {
+        NamePrompt: string;
+        LocationPrompt: string;
+        HaveLocation: string;
+        AddLinkedAccountsMessage: string;
+    } = {
+        NamePrompt: 'namePrompt',
+        LocationPrompt: 'locationPrompt',
+        HaveLocation: 'haveLocation',
+        AddLinkedAccountsMessage: 'linkedAccountsInfo'
     };
 
-    // Declare the responses map prompts
-    private static readonly RESPONSE_TEMPLATES: LanguageTemplateDictionary = new Map([
-        ['default', new Map([
+    private static readonly responseTemplates: LanguageTemplateDictionary = new Map([
+        ['default', <TemplateIdMap> new Map([
+            [OnboardingResponses.responseIds.NamePrompt, OnboardingResponses.fromResources('onBoarding.namePrompt')],
+            [OnboardingResponses.responseIds.LocationPrompt, OnboardingResponses.fromResources('onBoarding.locationPrompt')],
+            [OnboardingResponses.responseIds.HaveLocation, OnboardingResponses.fromResources('onBoarding.haveLocation')],
+            [OnboardingResponses.responseIds.AddLinkedAccountsMessage,
+                // tslint:disable-next-line:no-any
+                (context: TurnContext, data: any): Promise<Activity> => OnboardingResponses.BUILD_LINKED_ACCOUNTS_CARD(context, data)]
         ])]
    ]);
 
-   // Initialize the responses class properties
+   // Constructor
     constructor() {
         super();
-        this.register(new DictionaryRenderer(OnboardingResponses.RESPONSE_TEMPLATES));
+        this.register(new DictionaryRenderer(OnboardingResponses.responseTemplates));
+    }
+
+    // tslint:disable-next-line:no-any
+    public static async BUILD_LINKED_ACCOUNTS_CARD(turnContext: TurnContext, data: any): Promise<Activity> {
+        const title: string = i18n.__('onBoarding.linkedAccountsInfoTitle');
+        const text: string = i18n.__('onBoarding.linkedAccountsInfoBody');
+        const images: (CardImage | string)[] = [
+            {
+                url: i18n.__('onBoarding.linkedAccountsInfoUrl'),
+                alt: i18n.__('onBoarding.linkedAccountsInfoAlt')
+            }
+        ];
+        const attachment: Attachment = CardFactory.heroCard(title, text, images);
+        const response: Partial<Activity> = MessageFactory.attachment(attachment, title, InputHints.AcceptingInput);
+
+        response.suggestedActions = {
+            actions: [
+            {
+                title: i18n.__('main.helpBtnText1'),
+                type: ActionTypes.ImBack,
+                value: i18n.__('main.helpBtnValue1')
+            },
+            {
+                title: i18n.__('main.helpBtnText2'),
+                type: ActionTypes.ImBack,
+                value: i18n.__('main.helpBtnValue2')
+            },
+            {
+                title: i18n.__('main.helpBtnText3'),
+                type: ActionTypes.OpenUrl,
+                value: i18n.__('main.helpBtnValue3')
+            }
+            ],
+            to: []
+        };
+
+        return Promise.resolve(<Activity> response);
     }
 
     private static fromResources(name: string): TemplateFunction {
         return (): Promise<string> => Promise.resolve(i18n.__(name));
     }
+
 }
