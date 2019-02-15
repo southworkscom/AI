34e8c5d35d26adc05d3036cfadda73af276d83b4
Enzo Cano <enzo.cano@southworks.com>
2019-02-15 10:07:14 -0300

    Add lib configuration for bot-solution

--- solutions/Virtual-Assistant/src/typescript/bot-solution/src/skills/index.ts
+++ solutions/Virtual-Assistant/src/typescript/bot-solution/src/skills/index.ts
@@ -1,8 +1,6 @@
 /**
- * @module bot-solution
- */
-/**
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
-export * from './skills';
+
+export { SkillDefinition } from './skillDefinition';
