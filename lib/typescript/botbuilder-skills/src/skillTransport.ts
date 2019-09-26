f53c04e89e817e99460e90cc33477d2d41cb63d4
VictorGrycuk <victor@grycuk.net>
2019-09-26 14:13:06 -0300

    Update SkillTransport

--- lib/typescript/botbuilder-skills/src/skillTransport.ts
+++ lib/typescript/botbuilder-skills/src/skillTransport.ts
@@ -5,11 +5,21 @@
 
 import { TurnContext } from 'botbuilder';
 import { Activity } from 'botframework-schema';
+import { ISkillManifest } from './models';
+import { IServiceClientCredentials } from './auth';
 
 export type TokenRequestHandler = (activity: Activity) => Promise<void>;
+export type FallbackHandler = (activity: Activity) => Promise<void>;
 
 export interface ISkillTransport {
-    forwardToSkill(turnContext: TurnContext, activity: Partial<Activity>, tokenRequestHandler?: TokenRequestHandler): Promise<boolean>;
-    cancelRemoteDialogs(turnContext: TurnContext): Promise<void>;
+    forwardToSkill(
+        skillManifest: ISkillManifest,
+        serviceClientCredentials: IServiceClientCredentials,
+        turnContext: TurnContext,
+        activity: Partial<Activity>,
+        tokenRequestHandler?: TokenRequestHandler,
+        fallbackHandler?: FallbackHandler
+    ): Promise<Partial<Activity>>;
+    cancelRemoteDialogs(skillManifest: ISkillManifest, appCredentials: IServiceClientCredentials, turnContext: TurnContext): Promise<void>;
     disconnect(): void;
 }
