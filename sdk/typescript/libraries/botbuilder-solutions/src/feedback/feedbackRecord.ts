b92222b30a1a33df5af43b419cc3aaf4384123bb
Cristian Ledesma <cristian.ledesma@southworks.com>
2019-10-09 17:07:36 -0300

    Add feedbackRedord.ts initial implementation

--- sdk/typescript/libraries/botbuilder-solutions/src/feedback/feedbackRecord.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/feedback/feedbackRecord.ts
@@ -1,4 +1,39 @@
+import { Activity } from 'botframework-schema';
+
 /**
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
+export class FeedbackRecord {
+    /** 
+    * Gets or sets the activity for which feedback was requested.
+    * <value>
+    * The activity for which feedback was requested.
+    * </value>
+    */
+    public request?: Activity;
+
+    /**
+    * Gets or sets feedback value submitted by user.
+    * <value>
+    * Feedback value submitted by user.
+    * </value>
+    */
+    public feedback: string = '';
+
+    /**
+    * Gets or sets free-form comment submitted by user.
+    * <value>
+    * Free-form comment submitted by user.
+    * </value>
+    */
+    public comment: string = '';
+
+    /** 
+    * Gets or sets tag for categorizing feedback.
+    * <value>
+    * Tag for categorizing feedback.
+    * </value>
+    */
+    public tag: string = '';
+}
