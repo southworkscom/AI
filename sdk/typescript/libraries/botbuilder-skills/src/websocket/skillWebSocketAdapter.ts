b48eaa74e5386aec83ef15527a0e0900f17ec7c7
Diego Cardozo <42946572+DiegoCardozo94@users.noreply.github.com>
2019-10-10 10:47:04 -0300

    [TypeScript][BotBuilder Skills] Update skillWebSocketAdapter (#57)
    * Update files
    
    * Update skillWebSocketAdapter
    
    * Remove classes that we are not going to migrate at this moment
    
    * Start to fixes the changes of the migration
    
    * Finish to migrate SkillWebSocketAdapter
    
    * Remove extends to BotFrameworkAdapter
    
    * Fix remaining ESLint issue

--- sdk/typescript/libraries/botbuilder-skills/src/websocket/skillWebSocketAdapter.ts
+++ sdk/typescript/libraries/botbuilder-skills/src/websocket/skillWebSocketAdapter.ts
@@ -1,8 +1,14 @@
-import { BotFrameworkAdapter, BotFrameworkAdapterSettings, BotTelemetryClient, NullTelemetryClient,
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { BotTelemetryClient, NullTelemetryClient,
     TurnContext, WebRequest, WebResponse } from 'botbuilder';
+import { IBotSettingsBase} from 'botbuilder-solutions';
 import { ISocket, WebSocketServer } from 'botframework-streaming-extensions';
 import { BotCallbackHandler } from '../activityHandler';
-import { IAuthenticationProvider } from '../auth';
+import { IAuthenticationProvider, MsJWTAuthenticationProvider } from '../auth';
 import { SkillWebSocketBotAdapter } from './skillWebSocketBotAdapter';
 import { SkillWebSocketRequestHandler } from './skillWebSocketRequestHandler';
 
@@ -13,40 +19,69 @@ import { SkillWebSocketRequestHandler } from './skillWebSocketRequestHandler';
  * 2. Create RequestHandler to handle follow-up websocket frames.
  * 3. Start listening on the websocket connection.
  */
-export class SkillWebSocketAdapter extends BotFrameworkAdapter {
-    private readonly authenticationProvider?: IAuthenticationProvider;
+export class SkillWebSocketAdapter {
     private readonly telemetryClient: BotTelemetryClient;
-    private readonly botAdapter: SkillWebSocketBotAdapter;
+    private readonly skillWebSocketBotAdapter: SkillWebSocketBotAdapter;
+    private readonly botSettingsBase: IBotSettingsBase;
+    private readonly authenticationProvider?: IAuthenticationProvider;
 
     public constructor(
-        botAdapter: SkillWebSocketBotAdapter,
-        authenticationProvider?: IAuthenticationProvider,
-        telemetryClient?: BotTelemetryClient,
-        config?: Partial<BotFrameworkAdapterSettings>
+        skillWebSocketBotAdapter: SkillWebSocketBotAdapter,
+        botSettingsBase: IBotSettingsBase,
+        telemetryClient?: BotTelemetryClient
     ) {
-        super(config);
-        this.botAdapter = botAdapter;
-        this.authenticationProvider = authenticationProvider;
+        if (skillWebSocketBotAdapter === undefined) { throw new Error('skillWebSocketBotAdapter has no value'); }
+        if (botSettingsBase === undefined) { throw new Error('botSettingsBase has no value'); }
+        this.skillWebSocketBotAdapter = skillWebSocketBotAdapter;
+        this.botSettingsBase = botSettingsBase;
+        this.authenticationProvider = new MsJWTAuthenticationProvider(this.botSettingsBase.microsoftAppId);
         this.telemetryClient = telemetryClient || new NullTelemetryClient();
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/tslint/config
-    public async processActivity(req: WebRequest, res: WebResponse, logic: (context: TurnContext) => Promise<any>): Promise<void> {
-        await this.createWebSocketConnection(req, logic);
+    public async processActivity(req: WebRequest, res: WebResponse, bot: (context: TurnContext) => Promise<any>): Promise<void> {
+        if (req === undefined) { throw new Error('request has no value'); }
+        if (res === undefined) { throw new Error('response has no value'); }
+        if (bot === undefined) { throw new Error('bot has no value'); }
+        //PENDING
+        // if (!httpRequest.HttpContext.WebSockets.IsWebSocketRequest)
+        // {
+        //     httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+        //     await httpResponse.WriteAsync("Upgrade to WebSocket required.").ConfigureAwait(false);
+        //     return;
+        // }
+        await this.createWebSocketConnection(req, bot);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/tslint/config
     private async createWebSocketConnection(req: any, bot: BotCallbackHandler): Promise<void> {
-        // MISSING found an equivalent to websocket in TypeScript
         const socket: ISocket = req.socket;
         const handler: SkillWebSocketRequestHandler = new SkillWebSocketRequestHandler(this.telemetryClient);
         const server: WebSocketServer = new WebSocketServer(socket, handler);
-        // MISSING the Server class does not exposes Disconnected handler
-        // in C# server.Disconnected += Server_Disconnected;
-        this.botAdapter.server = server;
+        this.skillWebSocketBotAdapter.server = server;
         handler.bot = bot;
-        handler.skillWebSocketBotAdapter = this.botAdapter;
+        handler.skillWebSocketBotAdapter = this.skillWebSocketBotAdapter;
+        let latency: number = 0;
+        try {
+            const begin: [number, number] = process.hrtime();
+            await server.start();
+            const end: [number, number] = process.hrtime(begin);
+            latency = toMilliseconds(end);
 
-        await server.start();
+        } catch (error) {
+            throw new Error('Callback failed');
+        }
+        const latencyMetrics: { latency: number } = { latency: latency };
+        const event: string = 'Starting listening on websocket';
+        this.telemetryClient.trackEvent({
+            name: event,
+            metrics: latencyMetrics
+        });
     }
+}
+
+function toMilliseconds(hrtime: [number, number]): number {
+    const nanoseconds: number = (hrtime[0] * 1e9) + hrtime[1];
+
+    return nanoseconds / 1e6;
 }
