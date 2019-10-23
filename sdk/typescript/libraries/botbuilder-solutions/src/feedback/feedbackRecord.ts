00b171687e6d8b45bf7c249205b8120a59887d5d
Martin Battaglino <martinbatta32@gmail.com>
2019-10-23 12:03:05 -0300

    Update comments, and make nullables all the properties

--- sdk/typescript/libraries/botbuilder-solutions/src/feedback/feedbackRecord.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/feedback/feedbackRecord.ts
@@ -7,33 +7,25 @@ import { Activity } from 'botframework-schema';
 export class FeedbackRecord {
     /** 
     * Gets or sets the activity for which feedback was requested.
-    * <value>
     * The activity for which feedback was requested.
-    * </value>
     */
     public request?: Activity;
 
     /**
     * Gets or sets feedback value submitted by user.
-    * <value>
     * Feedback value submitted by user.
-    * </value>
     */
-    public feedback: string = '';
+    public feedback?: string;
 
     /**
     * Gets or sets free-form comment submitted by user.
-    * <value>
     * Free-form comment submitted by user.
-    * </value>
     */
-    public comment: string = '';
+    public comment?: string;
 
     /** 
     * Gets or sets tag for categorizing feedback.
-    * <value>
     * Tag for categorizing feedback.
-    * </value>
     */
-    public tag: string = '';
+    public tag?: string;
 }
