3e657d595877b4f9d3ba5acefba6375d81ebb98c
VictorGrycuk <victor@grycuk.net>
2019-10-24 14:22:35 -0300

    Remove unused parameter

--- sdk/typescript/libraries/botbuilder-solutions/src/testing/defaultTestAdapter.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/testing/defaultTestAdapter.ts
@@ -2,11 +2,11 @@
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
-import { TestAdapter, TurnContext  } from 'botbuilder';
+import { TestAdapter } from 'botbuilder';
 import { EventDebuggerMiddleware } from '../middleware';
 
 export class DefaultTestAdapter extends TestAdapter {
-    constructor(context: TurnContext) {
+    constructor() {
         // tslint:disable-next-line: no-empty
         super(async() => {});
         super.use(new EventDebuggerMiddleware());
