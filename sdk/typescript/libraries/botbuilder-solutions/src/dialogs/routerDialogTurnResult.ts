37d8b87854dbd6181362490a9ecb12d4cfe9521a
VictorGrycuk <victor@grycuk.net>
2019-10-25 12:02:44 -0300

    Parity and implementation of 'dialogs' folder
    - Add routerDialogTurnResult.ts
    - Add routerDialogTurnStatus.ts
    - Implementation on routerDialog.ts

---
+++ sdk/typescript/libraries/botbuilder-solutions/src/dialogs/routerDialogTurnResult.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { RouterDialogTurnStatus } from './routerDialogTurnStatus';
+
+export class RouterDialogTurnResult {
+
+    constructor(status: RouterDialogTurnStatus) {
+        this.status = status;
+    }
+
+    public status: RouterDialogTurnStatus;
+}
