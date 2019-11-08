af64d772a8e18ceb89cdc828a16547775e79f51e
Diego Cardozo <42946572+DiegoCardozo94@users.noreply.github.com>
2019-11-08 10:11:48 -0300

    [TypeScript][BotBuilder-Skills] Update root folder (#84)
    * Update root folder
    
    * Update index and Websocket folder
    
    * Update root files
    
    * Update websocket folder
    
    * Update index files of Skills (Template and Sample)
    
    * Rollback of websocket and protocol changes
    
    * Parity fo SkillTransport and SkillDialog classes

--- sdk/typescript/libraries/botbuilder-skills/src/index.ts
+++ sdk/typescript/libraries/botbuilder-skills/src/index.ts
@@ -21,3 +21,7 @@ export * from './skillManifestGenerator';
 export * from './skillMiddleware';
 export * from './skillRouter';
 export * from './skillTransport';
+export * from './skillConstants';
+export * from './skillDialogOption';
+export * from './skillIntentRecognizer';
+export * from './skillSwitchConfirmOption';
--- sdk/typescript/libraries/botbuilder-skills/src/skillTransport.ts
+++ sdk/typescript/libraries/botbuilder-skills/src/skillTransport.ts
@@ -5,10 +5,23 @@
 
 import { TurnContext } from 'botbuilder';
 import { Activity } from 'botframework-schema';
+import { IServiceClientCredentials } from './auth';
+import { ISkillManifest } from './models';
 
 export type TokenRequestHandler = (activity: Activity) => Promise<void>;
+export type FallbackHandler = (activity: Activity) => Promise<void>;
 
 export interface ISkillTransport {
+    // PENDING: both method should be uncommented when the WS library is merged
+    // forwardToSkill(
+    //     skillManifest: ISkillManifest,
+    //     serviceClientCredentials: IServiceClientCredentials,
+    //     turnContext: TurnContext,
+    //     activity: Partial<Activity>,
+    //     tokenRequestHandler?: TokenRequestHandler,
+    //     fallbackHandler?: FallbackHandler
+    // ): Promise<Partial<Activity>>;
+    // cancelRemoteDialogs(skillManifest: ISkillManifest, appCredentials: IServiceClientCredentials, turnContext: TurnContext): Promise<void>;
     forwardToSkill(turnContext: TurnContext, activity: Partial<Activity>, tokenRequestHandler?: TokenRequestHandler): Promise<boolean>;
     cancelRemoteDialogs(turnContext: TurnContext): Promise<void>;
     disconnect(): void;
