4297da59a5305f38117681d78336c2e428aa2e01
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-12-26 14:51:04 -0300

    Update SkillDialogTest to 0.7

--- sdk/typescript/libraries/botbuilder-skills/test/helpers/skillDialogTest.js
+++ sdk/typescript/libraries/botbuilder-skills/test/helpers/skillDialogTest.js
@@ -7,7 +7,7 @@ const { SkillDialog } = require('../../lib/skillDialog');
 
 class SkillDialogTest extends SkillDialog {
     constructor(skillManifest, appCredentials, telemetryClient, skillContextAccessor, skillTransport){
-        super(skillManifest, appCredentials, telemetryClient, skillContextAccessor, undefined, undefined, skillTransport);
+        super(skillManifest, appCredentials, telemetryClient, skillContextAccessor, undefined, skillTransport);
     }
 }
 
