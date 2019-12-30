18700777d872ad5d0bf322c0132fd1eed3b5e409
DiegoCardozo94 <diego.cardozo@southworks.com>
2019-12-30 11:05:54 -0300

    Add models folder

---
+++ sdk/typescript/libraries/botbuilder-solutions/src/models/openDefaultApp.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export class OpenDefaultApp {
+    public MeetingUri?: string;
+
+    public TelephoneUri?: string;
+
+    public MapsUri?: string;
+
+    public MusicUri?: string;
+}
