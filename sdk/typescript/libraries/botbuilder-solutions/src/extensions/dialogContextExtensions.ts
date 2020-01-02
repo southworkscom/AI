4caeee950c62c3c0ee8353fa341363c042771b77
DiegoCardozo94 <diego.cardozo@southworks.com>
2020-01-02 18:23:05 -0300

    Update extensions folder

---
+++ sdk/typescript/libraries/botbuilder-solutions/src/extensions/dialogContextExtensions.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+ import { DialogContext } from 'botbuilder-dialogs';
+
+ export namespace suppressDialog {
+    export const suppressDialogCompletionKey : string = 'suppressDialogCompletionMessage';
+    
+    /**
+     * Provides an extension method to DialogContext enabling a Dialog to indicate whether it wishes to suppress any dialog
+     * completion method if for example it's handled itself or the operation (e.g. cancel, repeat) doesn't call for it.
+     * @param dc DialogContext
+     * @param suppress Boolean indicating whether any automatic dialog completion message should be suppressed.
+     */
+    export function suppressCompletionMessage(dc: DialogContext, suppress: Boolean): void {
+
+        if (dc === undefined) {
+            throw new Error('DialogContext is undefined');
+        } else {
+            suppress = dc.context.turnState.get(suppressDialogCompletionKey);
+        }
+    }
+    /**
+     * Provides an extension method to DialogContext enabling the caller to retrieve whether it should suppress a dialog completion message.
+     * @param dc DialogContext
+     * @returns Indicates whether a dialog completion message should be sent.
+     */
+    export function suppressMessage(dc: DialogContext): Boolean {
+
+        if (dc === undefined){
+            throw new Error('DialogContext is undefined');
+        } else if (dc.context.turnState.has(suppressDialogCompletionKey)) {
+                return <Boolean>dc.context.turnState.get(suppressDialogCompletionKey);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ sdk/typescript/libraries/botbuilder-solutions/src/extensions/luisRecognizerExtensions.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+ import { SentimentType } from '../models';
+
+export namespace LuisRecognizerExtensions {
+
+    export const sentiment: string = "sentiment";
+    export const positiveSentiment: string = "positive";
+    export const neutralSentiment: string = "neutral";
+    export const negativeSentiment: string = "negative";
+
+    export function getSentimentInfo<T>(luisConverter: <T>() => any, propertyAccessor: (value: T) => Map<string, Object>): (score: number, label: SentimentType) => {
+        let sentimentLabel: SentimentType = SentimentType.None;
+        let maxScore: number = 0.0;
+
+        const luisProperty: Map<string, Object> = propertyAccessor(luisConverter);
+        const result: Object | undefined = luisProperty.get(sentiment);
+
+        if(luisProperty !== undefined && result !== undefined) {
+            let sentimentInfo: SentimentType = JSON.parse(<string>result);
+            sentimentLabel = getSentimentType(sentimentInfo.label);
+            maxScore = sentimentInfo.
+        }
+
+        return (sentimentLabel, maxScore);
+    }
+
+    export function getSentimentType (label: string): SentimentType {
+        let sentimentType: SentimentType = SentimentType.None;
+
+        if (label === positiveSentiment) {
+            sentimentType = SentimentType.Positive;
+        } else if (label === neutralSentiment) {
+            sentimentType = SentimentType.Neutral;
+        } else if (label === negativeSentiment) {
+            sentimentType = SentimentType.Negative;
+        }
+
+        return sentimentType;
+    }
+}
