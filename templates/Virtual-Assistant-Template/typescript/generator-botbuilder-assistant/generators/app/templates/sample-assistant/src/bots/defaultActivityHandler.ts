7c1adf998685a1977ac37776d9a42d1a06c8f4c5
Martin Battaglino <martinbatta32@gmail.com>
2020-01-14 13:18:59 -0300

    Update defaultActivityHandler

--- templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/sample-assistant/src/bots/defaultActivityHandler.ts
+++ templates/Virtual-Assistant-Template/typescript/generator-botbuilder-assistant/generators/app/templates/sample-assistant/src/bots/defaultActivityHandler.ts
@@ -3,52 +3,49 @@
  * Licensed under the MIT License.
  */
 
-import { TeamsActivityHandler, ConversationState, UserState, StatePropertyAccessor, BotTelemetryClient, TurnContext } from 'botbuilder';
-import { Dialog, DialogState, DialogSet } from 'botbuilder-dialogs';
-import { ChannelAccount } from 'botframework-schema';
+import { TeamsActivityHandler, ConversationState, UserState, StatePropertyAccessor, TurnContext } from 'botbuilder';
+import { Dialog, DialogState } from 'botbuilder-dialogs';
+import { DialogEx } from 'botbuilder-solutions';
 
 export class DefaultActivityHandler<T extends Dialog> extends TeamsActivityHandler {
-    private dialog: Dialog;
+    private readonly dialog: Dialog;
     private readonly conversationState: ConversationState;
     private readonly userState: UserState;
-    private readonly telemetryClient: BotTelemetryClient;
     private dialogStateAccessor: StatePropertyAccessor;
 
     public constructor (        
         conversationState: ConversationState,
         userState: UserState,
-        telemetryClient: BotTelemetryClient,
         dialog: T) {
         super();
         
-        this.userState = userState;
         this.dialog = dialog;
-        this.telemetryClient = telemetryClient;
         this.conversationState = conversationState;
+        this.userState = userState;
         this.dialogStateAccessor = conversationState.createProperty<DialogState>('DialogState');
     }
 
     public async turn(turnContext: TurnContext): Promise<any> {
-        await super.onTurn(turnContext);
+        await super.onTurnActivity(turnContext);
 
         // Save any state changes that might have occured during the turn.
         await this.conversationState.saveChanges(turnContext, false);
         await this.userState.saveChanges(turnContext, false);
     }
 
-    protected async onMembersAdded(membersAdded: ChannelAccount[], turnContext: () => TurnContext): Promise<any> {
-        return this.dialog.run(turnContext, this.dialogStateAccessor);
+    protected async onTeamsMembersAdded(turnContext: TurnContext): Promise<void> {
+        return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
 
-    protected async onMessageActivity(turnContext: () => TurnContext): Promise<any> {
-        return this.dialog.run(turnContext, this.dialogStateAccessor);
+    protected async onMessageActivity(turnContext: TurnContext): Promise<any> {
+        return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
 
-    protected async onTeamsSigninVerifyState(turnContext: () => TurnContext): Promise<any> {
-        return this.dialog.run(turnContext, this.dialogStateAccessor);
+    protected async onTeamsSigninVerifyState(turnContext: TurnContext): Promise<any> {
+        return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
 
-    protected async onEventActivity(turnContext: () => TurnContext): Promise<any> {
-        return this.dialog.run(turnContext, this.dialogStateAccessor);
+    protected async onEventActivity(turnContext: TurnContext): Promise<any> {
+        return DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
     }
 }
