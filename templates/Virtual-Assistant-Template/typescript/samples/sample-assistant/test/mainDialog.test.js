34313f18ef2c88946be69d3b8d433fc3df1e4aa9
DiegoCardozo94 <diego.cardozo@southworks.com>
2020-01-15 13:30:57 -0300

    Update mainDialogTests in samples

--- templates/Virtual-Assistant-Template/typescript/samples/sample-assistant/test/mainDialog.test.js
+++ templates/Virtual-Assistant-Template/typescript/samples/sample-assistant/test/mainDialog.test.js
@@ -6,45 +6,27 @@
 const assert = require('assert');
 const botTestBase = require('./helpers/botTestBase');
 const testNock = require('./helpers/testBase');
-const introJson = require('../src/content/NewUserGreeting.json');
-const introJsonEs = require('../src/content/NewUserGreeting.es-es.json');
+const {templateEngine, testUserProfileState } = require('./helpers/botTestBase');
 
 describe("Main Dialog", function () {
 	describe("Intro Card", function() {
 		it("Send conversationUpdate and verify card is received", function(done) {
 			botTestBase.getTestAdapterDefault().then((testAdapter) => {
-				const flow = testAdapter
-				.send({
-					type: "conversationUpdate",
-					membersAdded: [
-						{
-							id: "1",
-							name: "Bot"
-						}
-					],
-					channelId: "emulator",
-					recipient: {
-						id: "1"
-					}
-				})
+                const flow = testAdapter
+                .send({
+                    type: "conversationUpdate",
+                    membersAdded: [
+                        {
+                            id: "1",
+                            name: "user"
+                        }
+                    ],
+                })
 				.assertReply(function (activity, description) {
-					assert.strictEqual(activity.attachments[0].contentType, 'application/vnd.microsoft.card.adaptive');
-					assert.deepStrictEqual(activity.attachments[0].content, introJson);
+					assert.strictEqual(1, activity.attachments.length);
 				});
 
 				return testNock.resolveWithMocks('mainDialog_introCard_response', done, flow);
-			});
-		});
-	});
-
-	describe("Greeting", function () {
-		it("Send Hello and check you get the expected response", function (done) {
-			botTestBase.getTestAdapterDefault().then((testAdapter) => {
-				const flow = testAdapter
-					.send('Hi')
-					.assertReply('Hello.');
-
-				testNock.resolveWithMocks('mainDialog_greeting_response', done, flow);
 			});
 		});
 	});
@@ -54,7 +36,9 @@ describe("Main Dialog", function () {
 			botTestBase.getTestAdapterDefault().then((testAdapter) => {
 				const flow = testAdapter
 					.send('Help')
-					.assertReply('I\'m your Virtual Assistant! I can perform a number of tasks through my connected skills. Right now I can help you with Calendar, Email, Task and Point of Interest questions. Or you can help me do more by creating your own!');
+					.assertReply(function (activity, description) {
+						assert.strictEqual(1, activity.attachments.length);
+					});
 
 				testNock.resolveWithMocks('mainDialog_help_response', done, flow);
 			});
@@ -67,8 +51,7 @@ describe("Main Dialog", function () {
 				const flow = testAdapter
 					.send('I want to talk to a human')	
 					.assertReply(function (activity, description) {
-						assert.strictEqual(activity.attachments[0].contentType, 'application/vnd.microsoft.card.hero');
-						assert.deepStrictEqual(activity.attachments[0].content.title, 'Our agents are available 24/7 at 1(800)555-1234. Or connect with us through Microsoft Teams.');
+						assert.strictEqual(1, activity.attachments.length);
 					});
 
 				testNock.resolveWithMocks('mainDialog_escalate_response', done, flow);
@@ -85,18 +68,13 @@ describe("Main Dialog", function () {
 					membersAdded: [
 						{
 							id: "1",
-							name: "Bot"
+							name: "user"
 						}
 					],
-					channelId: "emulator",
-					recipient: {
-						id: "1"
-					},
 					locale: "es-es"
 				})
 				.assertReply(function (activity, description) {
-					assert.strictEqual(activity.attachments[0].contentType, 'application/vnd.microsoft.card.adaptive');
-					assert.deepStrictEqual(activity.attachments[0].content, introJsonEs);
+					assert.strictEqual(1, activity.attachments.length);
 				});
 
 				return testNock.resolveWithMocks('mainDialog_localization_response', done, flow);
@@ -106,24 +84,13 @@ describe("Main Dialog", function () {
 	
     describe("Confused", function () {
         it("Send an unhandled message", function (done) {
+			var allResponseVariations = templateEngine.templateEnginesPerLocale.get['en-us'].expandTemplate("UnsupportedMessage", testUserProfileState);
             botTestBase.getTestAdapterDefault().then((testAdapter) => {
                 const flow = testAdapter
                     .send('Unhandled message')
-                    .assertReply("I'm sorry, I'm not able to help with that.");
+                    .assertReply(allResponseVariations);
                     
                 testNock.resolveWithMocks('mainDialog_unhandled_response', done, flow);
-            });
-        });
-    });
-
-    describe("FAQ", function () {
-        it("Send a message with faq responses", function (done) {
-            botTestBase.getTestAdapterDefault().then((testAdapter) => {
-                const flow = testAdapter
-                    .send('What is a Virtual Assistant?')
-                    .assertReply("![Virtual Assistant](https://aka.ms/virtualassistantlogo)\nWe have seen significant need from our customers and partners to deliver a conversational assistant tailored to their brand, personalized to their customers and made available across a broad range of conversational canvases and devices. Continuing Microsoft open-sourced approach toward Bot Framework SDK, the open source Virtual Assistant solution provides full control over the end user experience built on a set of foundational capabilities. Additionally, the experience can be infused with intelligence about the end-user and any device/ecosystem information for a truly integrated and intelligent experience.\nFind out more [here](https://aka.ms/virtualassistant).");
-                
-                testNock.resolveWithMocks('mainDialog_faq_response', done, flow);
             });
         });
     });
