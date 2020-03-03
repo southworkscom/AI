1f636bf38a85596bbd2a5634cbf2b1c40210a315
VictorGrycuk <victor@grycuk.net>
2020-03-03 14:29:19 -0300

    Remove unnecessary import

--- templates/typescript/samples/sample-assistant/src/services/botSettings.ts
+++ templates/typescript/samples/sample-assistant/src/services/botSettings.ts
@@ -2,7 +2,7 @@
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
-import { IBotSettingsBase, IEnhancedBotFrameworkSkill } from 'botbuilder-solutions';
+import { IBotSettingsBase } from 'botbuilder-solutions';
 
 export interface IBotSettings extends IBotSettingsBase {
 }
