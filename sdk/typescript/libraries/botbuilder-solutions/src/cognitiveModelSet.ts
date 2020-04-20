44a9adf2881c26ca45aee17cde00bc7d7322831b
DiegoCardozo94 <diego.cardozo@southworks.com>
2020-04-20 16:15:42 -0300

    Change comment in cognitiveModelSet

--- sdk/typescript/libraries/botbuilder-solutions/src/cognitiveModelSet.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/cognitiveModelSet.ts
@@ -8,7 +8,7 @@ import { LuisRecognizerTelemetryClient, LuisRecognizer, QnAMaker, QnAMakerEndpoi
 export interface ICognitiveModelSet {
     dispatchService: LuisRecognizerTelemetryClient;
     luisServices: Map<string, LuisRecognizer>; 
-    // Please update your Virtual Assistant to use the new QnAMakerDialog with Multi Turn and Active Learning support instead. For more information, refer to https://aka.ms/bfvaqnamakerupdate.
+    //OBSOLETE: Please update your Virtual Assistant to use the new QnAMakerDialog with Multi Turn and Active Learning support instead. For more information, refer to https://aka.ms/bfvaqnamakerupdate.
     qnaServices: Map<string, QnAMaker>;
     qnaConfiguration: Map<string, QnAMakerEndpoint>;
 }
