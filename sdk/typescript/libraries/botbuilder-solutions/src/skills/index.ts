7f975fe33f2414af1ec68f925031efb12a1c9e88
DiegoCardozo94 <diego.cardozo@southworks.com>
2020-04-21 13:02:45 -0300

    Update index and add skillException

--- sdk/typescript/libraries/botbuilder-solutions/src/skills/index.ts
+++ sdk/typescript/libraries/botbuilder-solutions/src/skills/index.ts
@@ -7,6 +7,7 @@ export * from './dialogs';
 export * from './models';
 export * from './responses/skillResponses';
 export * from './skillDialog';
+export * from './skillException';
 export * from './skillMiddleware';
 export * from './skillConversationIdFactory';
 export * from './skillDialogArgs';
