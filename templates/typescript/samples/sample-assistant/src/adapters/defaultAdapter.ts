d4180115d1d83ffa58073011f0506dbb9c699a51
Martin Battaglino <martinbatta32@gmail.com>
2020-06-10 11:37:18 -0300

    Update DefaultAdapter, fixing typescript stuffs

--- templates/typescript/samples/sample-assistant/src/adapters/defaultAdapter.ts
+++ templates/typescript/samples/sample-assistant/src/adapters/defaultAdapter.ts
@@ -4,41 +4,39 @@
  */
 
 import {
-    ActivityTypes,
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
     BotTelemetryClient,
     ConversationState,
-    ShowTypingMiddleware, SkillHttpClient,
+    ShowTypingMiddleware,
+    SkillHttpClient,
     TranscriptLoggerMiddleware,
     TranscriptStore,
     TurnContext,
-    TelemetryException} from 'botbuilder';
+    TelemetryException } from 'botbuilder';
 import { AzureBlobTranscriptStore } from 'botbuilder-azure';
 import {
     EventDebuggerMiddleware,
     LocaleTemplateManager,
     SetLocaleMiddleware, 
-    SetSpeakMiddleware} from 'bot-solutions';
+    SetSpeakMiddleware } from 'bot-solutions';
 import { TelemetryInitializerMiddleware } from 'botbuilder-applicationinsights';
 import { IBotSettings } from '../services/botSettings.js';
-import {ActivityEx, SkillsConfiguration} from "bot-solutions/lib";
-import {AuthenticationConfiguration} from "botframework-connector";
-import {MainDialog} from "../dialogs/mainDialog";
-import { Activity, BotFrameworkSkill } from 'botbuilder-core';
+import { ActivityEx, SkillsConfiguration } from 'bot-solutions/lib';
+import { MainDialog } from '../dialogs/mainDialog';
+import { Activity, BotFrameworkSkill, IEndOfConversationActivity } from 'botbuilder-core';
 
 export class DefaultAdapter extends BotFrameworkAdapter {
 
     private readonly conversationState: ConversationState;
     private readonly telemetryClient: BotTelemetryClient;
-    private readonly templateManager: LocaleTemplateManager;
+    private readonly templateEngine: LocaleTemplateManager;
     private readonly skillClient: SkillHttpClient;
     private readonly skillsConfig: SkillsConfiguration;
 
     public constructor(
         settings: Partial<IBotSettings>,
-        authConfig: AuthenticationConfiguration,
-        templateManager: LocaleTemplateManager,
+        templateEngine: LocaleTemplateManager,
         conversationState: ConversationState,
         adapterSettings: Partial<BotFrameworkAdapterSettings>,
         telemetryMiddleware: TelemetryInitializerMiddleware,
@@ -48,9 +46,18 @@ export class DefaultAdapter extends BotFrameworkAdapter {
     ) {
         super(adapterSettings);
 
-        this.conversationState = conversationState ?? new Error('conversationState parameter is null');
-        this.templateManager = templateManager ?? new Error('templateManager parameter is null');
-        this.telemetryClient = telemetryClient ?? new Error('telemetryClient parameter is null');
+        if (conversationState === undefined) {
+            throw new Error('conversationState parameter is null');
+        }
+        this.conversationState = conversationState;
+        if (templateEngine === undefined) {
+            throw new Error('templateManager parameter is null');
+        }
+        this.templateEngine = templateEngine;
+        if (telemetryClient === undefined) {
+            throw new Error('telemetryClient parameter is null');
+        }
+        this.telemetryClient = telemetryClient;
         this.skillClient = skillClient;
         this.skillsConfig = skillsConfig;
 
@@ -70,43 +77,44 @@ export class DefaultAdapter extends BotFrameworkAdapter {
         // Uncomment the following line for local development without Azure Storage
         // this.use(new TranscriptLoggerMiddleware(new MemoryTranscriptStore()));
         this.use(new TranscriptLoggerMiddleware(transcriptStore));
-        this.use(new SetLocaleMiddleware(settings.defaultLocale || 'en-us'));
         this.use(new ShowTypingMiddleware());
+        this.use(new SetLocaleMiddleware(settings.defaultLocale || 'en-us'));
         this.use(new EventDebuggerMiddleware());
         this.use(new SetSpeakMiddleware());
     }
 
-    private async handleTurnError(context: TurnContext, error: Error): Promise<void> {
+    private async handleTurnError(turnContext: TurnContext, error: Error): Promise<void> {
         // Log any leaked exception from the application.
-        console.error(`unhandled error "${error.message}"`);
+        console.error(`[onTurnError] unhandled error : ${ error }`);
 
-        await this.sendErrorMessage(context, error);
-        await this.endSkillConversation(context);
-        await this.clearConversationState(context);
+        await this.sendErrorMessage(turnContext, error);
+        await this.endSkillConversation(turnContext);
+        await this.clearConversationState(turnContext);
     }
 
-    private async sendErrorMessage(context: TurnContext, error: Error): Promise<void> {
+    private async sendErrorMessage(turnContext: TurnContext, error: Error): Promise<void> {
         try {
             const telemetryException: TelemetryException = {
                 exception: error
-            }
+            };
 
             this.telemetryClient.trackException(telemetryException);
 
-            await context.sendActivity(this.templateManager.generateActivityForLocale("ErrorMessage"));
+            // Send a message to the user.
+            await turnContext.sendActivity(this.templateEngine.generateActivityForLocale('ErrorMessage'));
 
             // Send a trace activity, which will be displayed in the Bot Framework Emulator.
             // Note: we return the entire exception in the value property to help the developer;
             // this should not be done in production.
-            await context.sendTraceActivity("OnTurnError Trace", error.message, "https://www.botframework.com/schemas/error", "TurnError");
+            await turnContext.sendTraceActivity('onTurnError Trace', error.message, 'https://www.botframework.com/schemas/error', 'TurnError');
         }
-        catch (Exception) {
-            console.error(`Exception caught in SendErrorMessageAsync "${Exception}"`);
+        catch (err) {
+            console.error(`Exception caught in sendErrorMessage : "${ err }`);
         }
     }
 
-    private async endSkillConversation(context: TurnContext): Promise<void> {
-        if (this.skillClient == null || this.skillsConfig == null) {
+    private async endSkillConversation(turnContext: TurnContext): Promise<void> {
+        if (this.skillClient === undefined || this.skillsConfig === undefined) {
             return;
         }
 
@@ -114,30 +122,30 @@ export class DefaultAdapter extends BotFrameworkAdapter {
             // Inform the active skill that the conversation is ended so that it has a chance to clean up.
             // Note: the root bot manages the ActiveSkillPropertyName, which has a value while the root bot
             // has an active conversation with a skill.
-            const activeSkill = await this.conversationState.createProperty<BotFrameworkSkill>(MainDialog.activeSkillPropertyName).get(context);
-            if (activeSkill != null) {
-                let endOfConversation = ActivityEx.createEndOfConversationActivity();
-                endOfConversation.code = "RootSkillError";
-                endOfConversation = TurnContext.applyConversationReference(endOfConversation, TurnContext.getConversationReference(context.activity), true);
+            const activeSkill: BotFrameworkSkill | undefined = await this.conversationState.createProperty<BotFrameworkSkill>(MainDialog.activeSkillPropertyName).get(turnContext);
+            if (activeSkill !== undefined) {
+                let endOfConversation: Partial<IEndOfConversationActivity> = ActivityEx.createEndOfConversationActivity();
+                endOfConversation.code = 'RootSkillError';
+                endOfConversation = TurnContext.applyConversationReference(endOfConversation, TurnContext.getConversationReference(turnContext.activity), true);
 
-                await this.conversationState.saveChanges(context, true);
-                await this.skillClient.postToSkill<BotFrameworkSkill>(this.settings.appId, this.settings.appId, activeSkill, this.skillsConfig.skillHostEndpoint, <Activity>endOfConversation);
+                await this.conversationState.saveChanges(turnContext, true);
+                await this.skillClient.postToSkill(this.settings.appId, activeSkill, this.skillsConfig.skillHostEndpoint, endOfConversation as Activity);
             }
         }
-        catch (Exception) {
-            console.error(`Exception caught on attempting to send EndOfConversation: ${Exception}`);
+        catch (err) {
+            console.error(`Exception caught on attempting to send endOfConversation : ${ err }`);
         }
     }
 
-    private async clearConversationState(context: TurnContext): Promise<void> {
+    private async clearConversationState(turnContext: TurnContext): Promise<void> {
         try {
             // Delete the conversationState for the current conversation to prevent the
             // bot from getting stuck in a error-loop caused by being in a bad state.
             // ConversationState should be thought of as similar to "cookie-state" for a Web page.
-            await this.conversationState.delete(context);
+            await this.conversationState.delete(turnContext);
         }
-        catch (Exception) {
-            console.error(`Exception caught on attempting to Delete ConversationState: ${Exception}`);
+        catch (err) {
+            console.error(`Exception caught on attempting to Delete ConversationState : ${ err }`);
         }
     }
 }
