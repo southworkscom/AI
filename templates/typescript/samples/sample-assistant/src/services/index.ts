6e0ec00a792ff269bb8aca894c59e47e0e5e8be0
Victor <victor@grycuk.net>
2020-06-19 11:32:35 -0300

    Add index.ts

---
+++ templates/typescript/samples/sample-assistant/src/services/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './botServices';
+export * from './botSettings';
+export {DispatchlLuis,
+    GeneratedEntities as DispatchEntities,
+    GeneratedInstance as DispatchInstance,
+    GeneratedIntents as DispatchIntents} from './dispatchLuis';
+export {GeneralLuis,
+    GeneratedEntities as GeneralEntities,
+    GeneratedInstance as GeneralInstance,
+    GeneratedIntents as GeneralIntents } from './generalLuis';
