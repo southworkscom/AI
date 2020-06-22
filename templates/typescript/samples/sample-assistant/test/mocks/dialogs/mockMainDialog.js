34c8facee44be1703c7116d8e6ac4f1ab4b21013
matiasroldan6 <matias.roldan@southworks.com>
2020-06-22 16:42:00 -0300

    mockMainDialog port initial commit

---
+++ templates/typescript/samples/sample-assistant/test/mocks/dialogs/mockMainDialog.js
@@ -0,0 +1,87 @@
+"use strict";
+/**
+ * Copyright(c) Microsoft Corporation.All rights reserved.
+ * Licensed under the MIT License.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MainDialog = void 0;
+const botbuilder_ai_1 = require("botbuilder-ai");
+const botbuilder_dialogs_1 = require("botbuilder-dialogs");
+const bot_solutions_1 = require("bot-solutions");
+const botframework_schema_1 = require("botframework-schema");
+const stateProperties_1 = require("../models/stateProperties");
+const mainDialog = require("../../../lib/dialogs/mainDialog");
+const join = require('path').join;
+const readFileSync = require('fs').readFileSync;
+const testNock = require('../../helpers/testBase');
+/**
+ * Dialog providing activity routing and message/event processing.
+ */
+let MockMainDialog = /** @class */ (() => {
+    class MockMainDialog extends mainDialog.MainDialog {
+        constructor(services, templateManager, userProfileState, previousResponseAccessor, onBoardingDialog, switchSkillDialog, skillDialogs, skillsConfig, activeSkillProperty) {
+            super(MainDialog.name);
+            this.faqDialogId = 'faq';
+            this.services = services;
+            this.templateManager = templateManager;
+            this.skillsConfig = skillsConfig;
+            this.userProfileState = userProfileState;
+            this.previousResponseAccesor = previousResponseAccessor;
+            // Create state property to track the active skillCreate state property to track the active skill
+            this.activeSkillProperty = activeSkillProperty;
+            const steps = [
+                this.onBoardingStep.bind(this),
+                this.introStep.bind(this),
+                this.routeStep.bind(this),
+                this.finalStep.bind(this)
+            ];
+            this.addDialog(new botbuilder_dialogs_1.WaterfallDialog(MockMainDialog.name, steps));
+            this.addDialog(new botbuilder_dialogs_1.TextPrompt(botbuilder_dialogs_1.TextPrompt.name));
+            this.initialDialogId = MockMainDialog.name;
+            // Register dialogs
+            this.onBoardingDialog = onBoardingDialog;
+            this.switchSkillDialog = switchSkillDialog;
+            this.addDialog(this.onBoardingDialog);
+            this.addDialog(this.switchSkillDialog);
+            // Register skill dialogs
+            skillDialogs.forEach((dialog) => {
+                this.addDialog(dialog);
+            });
+            this._mockHttpHandler = testNock.resolveWithMocks('QnAMaker_NoAnswer');
+            super.tryCreateQnADialog(this.tryCreateQnADialog.bind(this));
+        }
+
+        /** Creates a QnAMaker dialog for the correct locale if it's not already present on the dialog stack.
+         * Virtual method enables test mock scenarios.
+         * @param knowledgebaseId - Knowledgebase Identifier.
+         * @param cognitiveModels - CognitiveModelSet configuration information.
+         * @returns QnAMakerDialog instance.
+         */
+        tryCreateQnADialog(knowledgebaseId, cognitiveModels) {
+            const qnaEndpoint = cognitiveModels.qnaConfiguration.get(knowledgebaseId);
+            if (qnaEndpoint === undefined) {
+                throw new Error(`Could not find QnA Maker knowledge base configuration with id: ${knowledgebaseId}.`);
+            }
+            // QnAMaker dialog already present on the stack?
+            if (this.dialogs.find(knowledgebaseId) === undefined) {
+                return new botbuilder_ai_1.QnAMakerDialog(qnaEndpoint.knowledgeBaseId, qnaEndpoint.endpointKey, qnaEndpoint.host, this.templateManager.generateActivityForLocale('UnsupportedMessage'), undefined, this.templateManager.generateActivityForLocale('QnaMakerAdaptiveLearningCardTitle').text, this.templateManager.generateActivityForLocale('QnaMakerNoMatchText').text, undefined, undefined, undefined, knowledgebaseId);
+            }
+            else {
+                return undefined;
+            }
+        }
+/*
+        getFilePath(fileName) {
+            return join(__dirname, '..', 'testData', fileName);
+        }
+
+        getResponse(fileName) {
+            const path = this.getFilePath(fileName);
+            return readFileSync(fileName, 'UTF8');
+        }*/
+    }
+    // Conversation state property with the active skill (if any).
+    MockMainDialog.activeSkillPropertyName = `${typeof (MockMainDialog).name}.ActiveSkillProperty`;
+    return MockMainDialog;
+})();
+exports.MockMainDialog = MockMainDialog;
