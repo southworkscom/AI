df973f65d9b68a9072a0d023362f91b5aa0c7bfb
Victor <victor@grycuk.net>
2020-07-15 16:12:26 -0300

    Reintroduce line that wasnt supposed to be removed

--- templates/typescript/samples/sample-skill/src/extensions/turnContextEx.ts
+++ templates/typescript/samples/sample-skill/src/extensions/turnContextEx.ts
@@ -9,6 +9,7 @@ import { SkillValidation, ClaimsIdentity } from 'botframework-connector';
 export namespace TurnContextEx {
 
     export function isSkill(turnContext: TurnContext): boolean {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const botIdentity = turnContext.turnState.get(turnContext.adapter.BotIdentityKey);
         return botIdentity instanceof ClaimsIdentity && SkillValidation.isSkillClaim(botIdentity.claims) ? true : false;
     }
