fd3117e41e83b5a5bee23cf07f8851c08ed26a76
Franco Alvarez <franco.alvarez@southworks.com>
2020-10-20 17:50:35 -0300

    Add unit test for VSIX project

---
+++ templates/csharp/BotTemplatesVSIX.Test/GeneratorVirtualAssistantTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BotTemplatesVSIX.Test
+{
+    [VsTestSettings(UIThread = true)]
+    public class GeneratorVirtualAssistantTest
+    {
+        private readonly ITestOutputHelper output;
+        private readonly string _vaProjectTemplatePath = @"..\..\..\..\VA\VA\VAProjectTemplate.vstemplate";
+
+        public GeneratorVirtualAssistantTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [VsFact]
+        public void Test_Generator_VA_Template()
+        {
+            XmlDocument templateFile = new XmlDocument();
+            templateFile.Load(_vaProjectTemplatePath);
+            XmlNodeList foldersList = templateFile.GetElementsByTagName("Folder");
+            output.WriteLine("The generator Bot Virtual Assistant template");
+            output.WriteLine("Should create the following folders: ");
+            foreach (XmlNode folder in foldersList)
+            {
+                output.WriteLine("\t {0}", folder.Attributes["TargetFolderName"].Value);
+            }
+
+            XmlNodeList filesList = templateFile.GetElementsByTagName("ProjectItem");
+            List<string> replacePlaceholderFiles = new List<string>();
+            List<string> notReplacePlaceholderFiles = new List<string>();
+            output.WriteLine("The following files: ");
+            foreach (XmlNode folder in filesList)
+            {
+                bool replaceParameters = bool.Parse(folder.Attributes["ReplaceParameters"].Value);
+                if (replaceParameters)
+                {
+                    replacePlaceholderFiles.Add(folder.Attributes["TargetFileName"].Value);
+                }
+                else
+                {
+                    notReplacePlaceholderFiles.Add(folder.Attributes["TargetFileName"].Value);
+                }
+            }
+
+            output.WriteLine("\t Will replace the placeholder: ");
+            foreach (string file in replacePlaceholderFiles)
+            {
+                output.WriteLine("\t \t {0}", file);
+            }
+
+            output.WriteLine("\t Will not replace the placeholder: ");
+            foreach (string file in notReplacePlaceholderFiles)
+            {
+                output.WriteLine("\t \t {0}", file);
+            }
+        }
+    }
+}
