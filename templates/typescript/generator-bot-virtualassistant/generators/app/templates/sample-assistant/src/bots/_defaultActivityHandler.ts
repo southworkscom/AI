3c9a96715f73a7c6850392af07cb55a1b203c512
matiasroldan6 <matias.roldan@southworks.com>
2020-12-02 16:48:29 -0300

    Add call to next in membersAdded, and parameter in signature

--- templates/typescript/generator-bot-virtualassistant/generators/app/templates/sample-assistant/src/bots/_defaultActivityHandler.ts
+++ templates/typescript/generator-bot-virtualassistant/generators/app/templates/sample-assistant/src/bots/_defaultActivityHandler.ts
@@ -60,7 +60,7 @@ export class DefaultActivityHandler<T extends Dialog> extends TeamsActivityHandl
         await this.userState.saveChanges(turnContext, false);
     }
 
-    protected async membersAdded(turnContext: TurnContext): Promise<void> {
+    protected async membersAdded(turnContext: TurnContext, next: () => Promise<void>): Promise<void> {
         const membersAdded: ChannelAccount[] = turnContext.activity.membersAdded;
         for (const member of membersAdded) {
             if (member.id !== turnContext.activity.recipient.id) {
@@ -78,6 +78,8 @@ export class DefaultActivityHandler<T extends Dialog> extends TeamsActivityHandl
                 await DialogEx.run(this.dialog, turnContext, this.dialogStateAccessor);
             }
         }
+        // By calling next() you ensure that the next BotHandler is run.
+        await next();
     }
 
     protected async onMessageActivity(turnContext: TurnContext): Promise<void> {
