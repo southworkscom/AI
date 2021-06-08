2ea450f4ae8984c83c9e5f0aac6b2444c9dd3063
Victor <victor@grycuk.net>
2021-06-08 10:52:27 -0300

    Update SetSpeakMiddleware voice name

--- samples/csharp/skill/SkillSample/Adapters/DefaultAdapter.cs
+++ samples/csharp/skill/SkillSample/Adapters/DefaultAdapter.cs
@@ -53,7 +53,7 @@ namespace SkillSample.Adapters
             Use(new ShowTypingMiddleware());
             Use(new SetLocaleMiddleware(settings.DefaultLocale ?? "en-us"));
             Use(new EventDebuggerMiddleware());
-            Use(new Microsoft.Bot.Builder.SetSpeakMiddleware("Microsoft Server Speech Text to Speech Voice (en-US, Jessa24kRUS)", true));
+            Use(new Microsoft.Bot.Builder.SetSpeakMiddleware("en-US-JennyNeural", true));
         }
 
         private async Task HandleTurnErrorAsync(ITurnContext turnContext, Exception exception)
