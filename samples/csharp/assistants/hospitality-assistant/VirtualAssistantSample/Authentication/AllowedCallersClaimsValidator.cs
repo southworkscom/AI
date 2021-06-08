f8a40d438f3660cddd5f91e67a32611a0d472523
Victor <victor@grycuk.net>
2021-06-08 15:26:31 -0300

    Update Hospitality Sample

--- samples/csharp/assistants/hospitality-assistant/VirtualAssistantSample/Authentication/AllowedCallersClaimsValidator.cs
+++ samples/csharp/assistants/hospitality-assistant/VirtualAssistantSample/Authentication/AllowedCallersClaimsValidator.cs
@@ -15,6 +15,7 @@ namespace VirtualAssistantSample.Authentication
     /// Sample claims validator that loads an allowed list from configuration if present
     /// and checks that responses are coming from configured skills.
     /// </summary>
+    [Obsolete("This class is deprecated, use BotBuilder's instead.")]
     public class AllowedCallersClaimsValidator : ClaimsValidator
     {
         private readonly List<string> _allowedSkills;
